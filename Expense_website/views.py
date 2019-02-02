1fe4f0b1d9e477ee14bd7a35db34a8276ea2bf43
anushiv <shuklanushiv@gmail.com>
2019-02-02 16:27:28 +0530

    udated sign up form

--- Expense_website/views.py
+++ Expense_website/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
-from .forms import signup_form
-from django.contrib.auth import authenticate,login,get_user_model,logout
-
-User=get_user_model()
 
 def index(request):
 	return render(request,'index.html',{})
@@ -25,20 +21,14 @@ def signin(request):
 	return render(request,'signin.html',{})
 
 def signup(request):
-	form_class=signup_form(request.POST or None)
-	context={
-		"form":form_class
-	}
-	if form_class.is_valid():
-		name=form_class.cleaned_data.get("name")
-		email=form_class.cleaned_data.get("email")
-		password=form_class.cleaned_data.get("password")
-		new_user= User.objects.create_user(email,email,password)
-		user_profile=profile(
-				user=new_user,
-				name=name,
-			)
-		user_profile.save()
-		if new_user is not None:
-			return redirect("/index")
-	return render(request,'signup.html',context)
+	if request.method == 'POST':
+		if request.POST['password'] == request.POST['password1']:
+			try:
+				user=User.objects.get(username=request.POST['name'])
+				return render(request,'signup.html',{'error':'Username is taken'})
+			except User.DoesNotExist:
+				user=User.objects.create_user(request.POST['name'], email=request.POST['email'], password=request.POST['password'])
+				auth.login(request, user)
+				return redirect('/index')
+	else:	
+		return render(request,'signup.html',{})
